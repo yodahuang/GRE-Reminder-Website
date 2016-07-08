86b51a6aa65014b1f02d8fffb85fe7acd8f4b38a
Huang <ward11sdo@qq.com>
2016-07-08 00:04:41 -0700

    WIndows sucks...

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,21 @@ var buffer = require('gulp-buffer');
 var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
 var ghPages = require('gulp-gh-pages');
-var browserSync = require("browser-sync").create();
+var browserSync = require('browser-sync').create();
 
-gulp.task('default', function () {
+gulp.task('browser-sync', function() {
+	gutil.log('Browser Sync!');
+    browserSync.init({
+        server: {
+            baseDir: "./src/html/"
+        }
+    });
+});
 
+gulp.task('release', function () {
 	gulp.src('src/html/*')
     	.pipe(gulp.dest('dist'))
-
-  	gulp.src('src/js/index.js', {read: false})
+  	return gulp.src('src/js/index.js', {read: false})
     	.pipe(tap(function (file) {
       		gutil.log('bundling ' + file.path);
       		file.contents = browserify(file.path, {debug: true}).transform("babelify", {presets: ["es2015"]}).bundle();
@@ -23,12 +30,6 @@ gulp.task('default', function () {
     	.pipe(uglify())
     	.pipe(sourcemaps.write('./'))
     	.pipe(gulp.dest('dist'));
-
-	browserSync.init({
-    	server: {
-        	baseDir: "./dist/"
-    	}
-	});
 });
 
 gulp.task('deploy', function() {
